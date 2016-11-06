d968d2f0614169a81bfdfa3851357cfdd8c7b8bd
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2016-11-06 16:51:28 -0700

    zip -> zipWith, zip2 -> zip.

--- src/Data/Zippable.purs
+++ src/Data/Zippable.purs
@@ -31,19 +31,19 @@ import Data.Tuple.Nested (Tuple3, tuple3, Tuple4, Tuple5)
 -- | zip (\x y -> f (g x) (h y)) v u == zip f (map g v) (map h u)
 -- | ```
 class (Functor f) <= Zippable f where
-  zip :: forall a b c. (a -> b -> c) -> (f a -> f b -> f c)
+  zipWith :: forall a b c. (a -> b -> c) -> (f a -> f b -> f c)
 
 instance arrayZippable :: Zippable Array where
-  zip f a1 a2 = map (\(Tuple x y) -> f x y) (A.zip a1 a2)
+  zipWith f a1 a2 = map (\(Tuple x y) -> f x y) (A.zip a1 a2)
 
-zip2 :: forall f a b. (Zippable f) => f a -> f b -> f (Tuple a b)
-zip2 = zip Tuple
+zip :: forall f a b. (Zippable f) => f a -> f b -> f (Tuple a b)
+zip = zipWith Tuple
 
 zip3 :: forall f a b c. (Zippable f) => f a -> f b -> f c -> f (Tuple3 a b c)
-zip3 u v w = zip (\x (Tuple y z) -> tuple3 x y z) u (zip2 v w)
+zip3 u v w = zipWith (\x (Tuple y z) -> tuple3 x y z) u (zip v w)
 
 zip4 :: forall f a b c d. (Zippable f) => f a -> f b -> f c -> f d -> f (Tuple4 a b c d)
-zip4 t u v w = zip Tuple t (zip3 u v w)
+zip4 t u v w = zipWith Tuple t (zip3 u v w)
 
 zip5 :: forall f a b c d e. (Zippable f) => f a -> f b -> f c -> f d -> f e -> f (Tuple5 a b c d e)
-zip5 s t u v w = zip Tuple s (zip4 t u v w)
+zip5 s t u v w = zipWith Tuple s (zip4 t u v w)
